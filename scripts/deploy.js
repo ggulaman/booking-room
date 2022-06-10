1a66d7531016394253b14db47e5bfcd3b8a0fd37
ggulaman <gulamman@protonmail.com>
2022-06-10 02:24:12 +0100

    deployment script update to write SC address on env files

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,18 @@
 const { ethers } = require("hardhat");
+const fs = require('fs');
 
 const network = process.env.HARDHAT_NETWORK;
+
+const addVariable = (path, variableName, address) => {
+  try {
+    fs.unlinkSync(path)
+    console.log(`Removing .env file on ${path}`);
+  } catch(err) {
+    console.log(`It seems ${path} doesn't exists`);
+  }
+  console.log(`Adding env. variable to ${path}`);
+  fs.appendFileSync(path, `${variableName}=${address}\n`);
+}
 const main = async() => {
     await hre.run('compile');
     const [deployer] = await ethers.getSigners();
@@ -10,6 +22,12 @@ const main = async() => {
     const ColaBookingFactory = await ethers.getContractFactory("ColaBookingFactory");
     const cbSC = await ColaBookingFactory.deploy();
     console.log(`ColaBookingFactory deployed to the following address ${cbSC.address}`);
+
+    const envFileServerPath = './colabooking-server/.env'
+    const envFileUIPath = './colabooking-ui/.env'
+
+    addVariable(envFileServerPath, 'COLADAY_SC_ADDRESS', cbSC.address);
+    addVariable(envFileUIPath, 'REACT_APP_COLA_DAY_SC_ADDRESS', cbSC.address);
 };
 
 main()
